c5078491e9f7fd61de43ef69ea82b32ef61fc9ab
daher7 <daher7@gmail.com>
2018-10-10 13:41:42 +0200

    Vida

---
+++ Assets/_GameAssets/Scripts/Items/CajaVida.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CajaVida : MonoBehaviour {
+
+    int rotacion = 0;
+    	
+	// Update is called once per frame
+	void Update () {
+        rotacion++;
+        transform.rotation = Quaternion.Euler(new Vector3(0, rotacion, 0));
+	}
+}

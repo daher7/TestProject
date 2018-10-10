3b74244d9caf69c099fec7e786cb9ee812a13b34
daher7 <daher7@gmail.com>
2018-10-10 13:58:18 +0200

    vida 1.0

--- Assets/_GameAssets/Scripts/Items/CajaVida.cs
+++ Assets/_GameAssets/Scripts/Items/CajaVida.cs
@@ -4,11 +4,33 @@ using UnityEngine;
 
 public class CajaVida : MonoBehaviour {
 
-    int rotacion = 0;
-    	
-	// Update is called once per frame
-	void Update () {
-        rotacion++;
-        transform.rotation = Quaternion.Euler(new Vector3(0, rotacion, 0));
+    [SerializeField] int speedRotation = 1;
+    int rotacionX = 0;
+    int rotacionY = 2;
+    int rotacionZ = 0;
+
+    int posMax;
+    int posMin;
+    int deltaY = 0;
+    int rotation = 0;
+    bool subiendo = true;
+
+    // Update is called once per frame
+    void Update () {
+      
+        //rotacionX = rotacionX + speedRotation;
+        rotacionY = rotacionY + speedRotation;
+        //rotacionZ = rotacionZ + speedRotation;
+
+        transform.rotation = Quaternion.Euler(new Vector3(rotacionX, rotacionY, rotacionZ));
+        if (subiendo) {
+            deltaY++;
+            transform.Translate(Vector3.up * Time.deltaTime * -1);
+        }
+        if(deltaY > 50) {
+            subiendo = false;
+        } else if (deltaY <= 0) {
+            subiendo = true;
+        }
 	}
 }

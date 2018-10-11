1b9c2df2e3ca43961e452c5ee08d98ea9582e9d6
daher7 <daher7@gmail.com>
2018-10-11 13:54:21 +0200

    Caja Vida V 1.0

--- Assets/_GameAssets/Scripts/Items/CajaVida.cs
+++ Assets/_GameAssets/Scripts/Items/CajaVida.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class CajaVida : MonoBehaviour {
 
     [SerializeField] int speedRotation = 1;
-    int rotacionX = 0;
+    [SerializeField] int vida = 1;
+
+    int rotacionX = 1;
     int rotacionY = 2;
-    int rotacionZ = 0;
+    int rotacionZ = 1;
 
     int posMax;
     int posMin;
-    int deltaY = 0;
+    
     int rotation = 0;
+
     bool subiendo = true;
+    int deltaY = 0;
 
     // Update is called once per frame
     void Update () {
@@ -27,10 +31,24 @@ public class CajaVida : MonoBehaviour {
             deltaY++;
             transform.Translate(Vector3.up * Time.deltaTime * -1);
         }
+
         if(deltaY > 50) {
             subiendo = false;
         } else if (deltaY <= 0) {
             subiendo = true;
         }
 	}
+
+    private void OnTriggerEnter(Collider other) {
+
+        if (other.gameObject.CompareTag("Player")) {
+            Personaje p = other.gameObject.GetComponent<Personaje>();
+            p.IncrementarVida(vida);
+
+            // Cuando recogemos el botiquin, este desaparece
+            Destroy(this.gameObject);
+        }
+    }
 }
+
+
